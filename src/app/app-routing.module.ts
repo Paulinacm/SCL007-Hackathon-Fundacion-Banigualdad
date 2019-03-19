5cbe2a21319b6d2dfa35ac6e333ffab3f9abf3d5
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-03-19 10:52:56 -0300

    Creando login basico, y otras dependencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ProfileComponent  } from './components/users/profile/profile.component';
+import { RegisterComponent } from './components/users/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users/login', component: LoginComponent},
+  {path: 'users/profile', component: ProfileComponent},
+  {path: 'users/regiter', component: RegisterComponent},
+  {path: 'page404', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/users/login/login.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
 import { RegisterComponent } from './components/users/register/register.component';
 import { Page404Component } from './components/page404/page404.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './components/home/home.component';
     ProfileComponent,
     RegisterComponent,
     Page404Component,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
