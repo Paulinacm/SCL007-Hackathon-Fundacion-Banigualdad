a646adcfd4fa55c7b0c247f71510984be22d09d5
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-03-22 02:06:44 -0300

    Agregando funcionalidad busqueda por Barbara y Kathy integrada por Scarlett

---
+++ src/js/app.js
@@ -0,0 +1,127 @@
+const emprendedor = [
+  {
+    "categoria": "Producto",
+    "ciudad": "Santiago",
+    "descripcion": "En el mes del niño, una de nuestras microempresarias destacadas es Patricia Abarca, creativa emprendedora de la comuna de El Bosque que se dedica a las costuras y que ha encontrado un nuevo nicho de negocio en ellas: la creación de gorros temáticos para los “gamers”, como se les llama a los aficionados a los video juegos. Ellos encuentran en Patricia una gran alternativa para poder vestirse como sus virtuales personajes favoritos.\n\n“Esto partió por una vecina mía que va a eventos y ferias de este tipo, la Festigames, la ComicCon. Ella me mostró una foto de un personaje y me preguntó si podía sacar el gorro. ¡Me gustó el desafío y lo hice! Después me empecé a tirar con otros modelos. Los que me los piden son los chiquillos que conocen los juegos, son para un público específico. También hago trajes de Mario Bros, de alguna princesa del mismo juego, lo que me pidan, porque me gusta que me encarguen cosas difíciles, ponerme pruebas y superarlas. A Banigualdad entré porque me invitaron, probé partiendo con poquito y he hecho súper buenas inversiones para mi negocio. Me compré una máquina de coser industrial, una overlock. Gracias a este trabajo puedo apoyar económicamente en mi casa y a mis hijos que están estudiando. Me gusta estar en Banigualdad y compartir con mi Centro”.",
+    "id": 1553212758122,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2017/08/patricia-abarca-1.jpg",
+    "nombre": "PATRICIA ABARCA",
+    "producto": ["Gorros"],
+    "whatsaap": "+56912453456",
+    "key": "1553212758122"
+  },
+  {
+    "categoria": "Producto",
+    "ciudad": "Valparaíso",
+    "descripcion": "“Soy una de las fundadoras de Banigualdad en Rinconada de Silva, mi Centro se llama “La amistad”. Decidí emprender porque necesitaba surgir, así que empecé a preparar arrollado y pan amasado en horno de barro, productos que aprendí a elaborar con las recetas de mis padres y mis abuelos. Gracias a Dios me ha ido muy bien, la gente se ha acostumbrado a mis productos. Como pertenezco a un grupo de artesanos desde hace 3 años, tenemos una feria los domingos en la plaza de Rinconada o a veces vamos a Putaendo que está cerquita. Uno se siente satisfecha como emprendedora, de la labor que hace y da satisfacción porque a la gente le gustan mis productos, de hecho me buscan por el pan amasado en horno de barro. Me siento bien aportando a los gastos de mi casa, en Banigualdad me he sentido bien, es agradable y la plata me ha servido para invertir en mis cosas: sacos de harina, me abastezco de carne para preparar los arrollados y así”.",
+    "id": 1553212758123,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2017/09/ernestina-cruz-3.jpg",
+    "nombre": "ERNESTINA CRUZ",
+    "producto": ["Pan amasado", "Arrollado"],
+    "whatsaap": "+569121256",
+    "key": "1553212758123"
+  },
+  {
+    "categoria": "Comercio",
+    "ciudad": "Maule",
+    "descripcion": "“Estoy hace 2 años en la Fundación, mi grupo fue el primero que se hizo en Maule, en la VII Región. Mi negocio es la venta de colaciones, aquí se vende la rica cazuela de vacuno todos los días, también hago porotitos, pollo arvejado, mote con huesillo en el verano y los completos que salen todos los días. También decidí hacer mi propia empresa de eventos, hago matrimonios, cenas, tengo de todo para 150 personas. Sillas, platos, mesas, mantelería, vajilla, todo lo que se necesita. Durante la semana atiendo mi negocio de colaciones y el fin de semana hago eventos. Estoy feliz y agradecida de Banigualdad por el apoyo, además de todo esto soy jefa de hogar, crié a mis cinco hijos. El apoyo de Banigualdad ha sido maravilloso, agradecida eternamente de la gente que nos está apoyando, de las capacitaciones. Hay que ir creciendo, a veces es difícil, pero aquí estamos aperrando. Me siento feliz de no depender de nadie y orgullosa de sacar mi familia adelante. No me veo sin trabajar”, nos dice Marisol Burgos.",
+    "id": 1553212758124,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2017/10/marisol-burgos.jpg",
+    "nombre": "MARISOL BURGOS",
+    "producto": ["Banquetería", "Colaciones", "Eventos"],
+    "whatsaap": "56923565445",
+    "key": "1553212758124"
+  },
+  {
+    "categoria": "Servicio",
+    "ciudad": "San Antonio",
+    "descripcion": "Alice Peñailillo vive en San Antonio, es ingeniero de profesión y luego de quedar sin trabajo el año pasado pudo dedicarse 100% a lo que más le gusta: “Todo lo que son las terapias complementarias me apasiona. Llevo 10 años trabajando a través de mis terapias, me he ido especializado, he participado en distintos cursos y talleres, hago terapias de reiki, de sanación. Siempre estaba en la búsqueda de algo, pensé que era un tema más religioso hasta que llegué a un curso de metafísica y ahí encontré lo que quería, comencé a estudiar y a hacer mis terapias. Esto genera un equilibrio en cuerpo, mente y alma. Además fabrico orgones, sacos de semilla, bolsitas terapéuticas con cuarzo. La Fundación fue re importante porque no contaba con el capital, al estar sin trabajo no tenía ningún respaldo. Apareció Banigualdad y fue magnífico, la capacitación y contención que me entrega nuestra asesora es fabulosa. Los materiales de capacitación son muy buenos y actuales”.",
+    "id": 1553216726116,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2018/06/alice-peailillo.jpg",
+    "nombre": "ALICE PEÑAILILLO",
+    "producto": ["Terapias", "Reiki"],
+    "whatsaap": "56978541259",
+    "key": "1553216726116"
+  },
+  {
+    "categoria": "Comercio",
+    "ciudad": "Maule",
+    "descripcion": "“Soy María Gómez, vivo en Maule, en el sector de El Mirador, en la VII Región. Mi emprendimiento son las brochetas, también hacemos tortillas, chicharrones, arrollado, queso cabeza, todo lo hago yo porque lo aprendí desde chica. Me pongo en una feria de sábado, domingo y feriados. Me gusta el negocio, toda mi familia es comerciante y todo lo aprendí de una persona muy especial que me enseñó”.\n\n“Entré a la Fundación porque vi un aviso en una sede, la Fundación sirve para la gente que tiene negocio, ganas de emprender, de decir que quiere hacer algo y lo hace. Coordino mi grupo hace 7 años, es excelente, siempre les digo a todos que aprovechen la oportunidad. Con Banigualdad aprendí lo que era la organización del negocio, a sacar los porcentajes, los libros son buenos, te enseñan”\n\n“Mi negocio ha crecido harto, de no tener nada pude hacer la pieza para poder trabajar, me compré un horno, mi máquina para revolver. He emprendido de a poquito, he sabido aprovechar esta gran oportunidad de Banigualdad”, nos dice María Cristina Gómez de la VII Región.\n\nArrollado, Brochetas, Chicharrones, Queso Cabeza, Tortillas",
+    "id": 1553216726117,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2017/11/m.cristinagomez.jpg",
+    "nombre": "MARIA GOMEZ",
+    "producto": ["Brochetas", "Tortillas", "Chicharrones", "Arrollado", "Quezo cabeza"],
+    "whatsaap": "56948751259",
+    "key": "1553216726117"
+  },
+  {
+    "categoria": "Producto",
+    "ciudad": "Santiago",
+    "descripcion": "“Tengo mi taller de costuras desde hace 9 años, la edad que tiene mi segundo hijo. Por él quise dejar de trabajar fuera de la casa y poder dedicarme a lo mío, a lo que más me gusta y a lo que estudié. Soy una mamá presente gracias a mi taller. En esta época estoy dedicada a los uniformes escolares, trabajo con cuatro colegios, hago las poleras, el pantalón, los buzos. Me gusta mucho todo lo que es confección de ropa. Luego de que termina la época escolar hago polerones para el invierno, abrigos de polar y ya en agosto nos preparamos para los trajes típicos de las Fiestas Patrias. En este taller trabajo con una compañera de Centro, nos conocimos en el grupo de la Fundación, ella tenía las mismas aspiraciones mías, así que se vino con todas sus cosas a mi casa. Estoy en el 3er ciclo en la Fundación. Una niña me invitó a participar y hasta ahora ha resultado bien. Banigualdad es un gran apoyo para los emprendedores, un apoyo que no tenemos”, nos dice Marianela Córdova, emprendedora de Padre Hurtado.",
+    "id": 1553216726118,
+    "imagen": "https://www.banigualdad.cl/wp-content/uploads/2018/03/marianela-crdova.jpg",
+    "nombre": "MARIANELA CORDOVA",
+    "producto": ["Uniformes escolares"],
+    "whatsaap": "56945784525",
+    "key": "1553216726118"
+  }
+];
+
+const em = emprendedor.forEach(function(element) {
+
+document.getElementById('emprendedores-list').innerHTML+=`  
+  <div class="col-4 mb-5">
+    <img class="imagenes" src="${element.imagen}" />
+    <h5 class="name-e">${element.nombre}</h5>
+   <p style="text-align">${element.ciudad}</p>
+    <p>
+      <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Ver Emprendedor
+      </a>
+    </p>
+    <div class="collapse" id="collapseExample">
+      <div class="card card-body">
+      <p class="description">${element.descripcion}</p>
+        ${element.producto} -  Contacto: <a href="https://api.whatsapp.com/send?phone=${element.whatsaap}">+${element.whatsaap}</a>
+      </div>
+    </div>
+  </div>
+` 
+});
+
+
+let searchInput = document.getElementById("searchInput");
+let searchResult = document.getElementById("searchResult");
+let searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", function () {
+  let inputValue = (searchInput.value).toUpperCase();
+  console.log(inputValue)
+  searchResult.innerHTML = "";
+  for (let i = 0; i < emprendedor.length; i++) {
+    if ((emprendedor[i].ciudad).toUpperCase() === inputValue || ((emprendedor[i].nombre).toUpperCase()).includes(inputValue) === true || ((emprendedor[i].producto).map(a => a.toUpperCase())).includes(inputValue) === true) {
+      document.getElementById('carouselExampleControls').style.display = "none";
+      document.getElementById('emprendedores-list').style.display ="none";
+      searchResult.innerHTML += `
+        <div class="col-4 mb-5">
+        <img class="imagenes" src="${emprendedor[i].imagen}" />
+        <h5 class="name-e">${emprendedor[i].nombre}</h5>
+       <p style="text-align">${emprendedor[i].ciudad}</p>
+        <p>
+          <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Ver Emprendedor
+          </a>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="card card-body">
+          <p class="description">${emprendedor[i].descripcion}</p>
+            ${emprendedor[i].producto} -  Contacto: <a href="https://api.whatsapp.com/send?phone=${emprendedor[i].whatsaap}">+${emprendedor[i].whatsaap}</a>
+          </div>
+        </div>
+      </div>      
+      `;
+    }
+  }
+});
+/**<input type="text" id="searchInput">
+  <button id="searchButton" class="btn btn-success mt-3 mb-3" type="submit">submit</button>
+  <div id="searchResult"></div> */
